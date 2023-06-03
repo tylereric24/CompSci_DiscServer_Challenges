17b4fbf2dabf1a55e569b9c30f0d712b713e097f
Jeff Sponenberg <grebj@proton.me>
2023-06-03 18:47:47 -0400

    added main method, added banner and tests

--- Coding Challenges/Challenge 4.py
+++ Coding Challenges/Challenge 4.py
@@ -1,27 +1,31 @@
-print("""
-  ________             ___.         .__                    _________              __                               _________ .__                   __                 
- /  _____/______   ____\_ |__  __ __|  |   ____   ____    /   _____/ ____   _____/  |_  ____   ____   ____  ____   \_   ___ \|  |__   ____   ____ |  | __ ___________ 
-/   \  __\_  __ \_/ __ \| __ \|  |  \  |  /  _ \ /    \   \_____  \_/ __ \ /    \   __\/ __ \ /    \_/ ___\/ __ \  /    \  \/|  |  \_/ __ \_/ ___\|  |/ // __ \_  __ \
-\    \_\  \  | \/\  ___/| \_\ \  |  /  |_(  <_> )   |  \  /        \  ___/|   |  \  | \  ___/|   |  \  \__\  ___/  \     \___|   Y  \  ___/\  \___|    <\  ___/|  | \/
- \______  /__|    \___  >___  /____/|____/\____/|___|  / /_______  /\___  >___|  /__|  \___  >___|  /\___  >___  >  \______  /___|  /\___  >\___  >__|_ \\___  >__|   
-        \/            \/    \/                       \/          \/     \/     \/          \/     \/     \/    \/          \/     \/     \/     \/     \/    \/       
-""")
+def main():
+    print_banner()
+    input_string = input("What sentence would you like to check? ")
+    print(f"The longest word is {sentence_splitter(input_string)}")
 
+def print_banner():
+    print("""
+    ________             ___.         .__                    _________              __                               _________ .__                   __                 
+    /  _____/______   ____\_ |__  __ __|  |   ____   ____    /   _____/ ____   _____/  |_  ____   ____   ____  ____   \_   ___ \|  |__   ____   ____ |  | __ ___________ 
+    /   \  __\_  __ \_/ __ \| __ \|  |  \  |  /  _ \ /    \   \_____  \_/ __ \ /    \   __\/ __ \ /    \_/ ___\/ __ \  /    \  \/|  |  \_/ __ \_/ ___\|  |/ // __ \_  __ \
+    \    \_\  \  | \/\  ___/| \_\ \  |  /  |_(  <_> )   |  \  /        \  ___/|   |  \  | \  ___/|   |  \  \__\  ___/  \     \___|   Y  \  ___/\  \___|    <\  ___/|  | \/
+    \______  /__|    \___  >___  /____/|____/\____/|___|  / /_______  /\___  >___|  /__|  \___  >___|  /\___  >___  >  \______  /___|  /\___  >\___  >__|_ \\___  >__|   
+            \/            \/    \/                       \/          \/     \/     \/          \/     \/     \/    \/          \/     \/     \/     \/     \/    \/       
+    """)
+    print()
 
-input_string = input("What sentence would you like to check? ")
-
-
-def sentence_splitter(input_string):
+def sentence_splitter(input_string): 
     split_string = input_string.split(" ")
-    print(split_string)
+    ##print(split_string)
     current_longest = ''
     for word in split_string:
         if len(word) > len(current_longest):
             current_longest = word
     return current_longest
+
+def test_sentence_splitter():
+    longest = sentence_splitter("I can't believe the Celtics choked.")
+    assert longest == "believe"
         
-
-
-
-
-print(f"The longest word is {sentence_splitter(input_string)}")
+if __name__ == '__main__':
+    main()
